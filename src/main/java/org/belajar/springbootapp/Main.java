c4395ecc2c2110662b2d68306ef42ca5af6f7f47
Piyan Stefani <103780843+piyandra@users.noreply.github.com>
2025-04-07 14:13:55 +0700

    Update Main.java

--- src/main/java/org/belajar/springbootapp/Main.java
+++ src/main/java/org/belajar/springbootapp/Main.java
@@ -5,7 +5,7 @@ import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
 public class Main {
 
-    static final String TOKEN = "7282002640:AAHJlmR52O_HDE_i-hjYI2QDK-VXG8aI9nY";
+    static final String TOKEN = "";
 
     public static void main(String[] args) {
         try (TelegramBotsLongPollingApplication botsLongPollingApplication = new TelegramBotsLongPollingApplication()){
