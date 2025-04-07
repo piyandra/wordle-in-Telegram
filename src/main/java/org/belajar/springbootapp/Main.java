39cc9a15db1fcc2f19e8ce4931b14c0f51114c19
piyan1212 <piyanstefani@gmail.com>
2025-04-07 14:29:03 +0700

    Assembly Plugin

--- src/main/java/org/belajar/springbootapp/Main.java
+++ src/main/java/org/belajar/springbootapp/Main.java
@@ -12,7 +12,6 @@ public class Main {
             botsLongPollingApplication.registerBot(TOKEN, new BotTebakHurufMain("7282002640:AAHJlmR52O_HDE_i-hjYI2QDK-VXG8aI9nY"));
             Thread.currentThread().join();
         } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
